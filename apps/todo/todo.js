3cb6bd32f3d7f69d583d5974a891abd649173296
dustinpfister <dustin.pfister@gmail.com>
2017-11-02 09:42:23 -0400

    todo app update

--- apps/todo/todo.js
+++ apps/todo/todo.js
@@ -1,40 +1,31 @@
 
-var Item = Backbone.Model.extend({
-
+var Item = Backbone.Model.extend(
+ 
+    {
+ 
         // these are the default values that will
         // be augmented with the object that is passed
         // to the constructor function
         defaults : {
-            title : '',
+            title : 'unknown',
             done : false
         },
-
-        // setup the item
-        set : function (obj) {
-
-            for (var prop in obj) {
-
-                this[prop] = obj[prop];
-
-            }
-
-        },
-
+ 
         // simple log method
         log : function () {
-
-            console.log('title: ' + this.title + '; done: ' + this.done);
-
+ 
+            console.log('title: ' + this.get('title') + '; done: ' + this.get('done'));
+ 
         }
-
-    });
-
+ 
+    }
+ 
+);
+ 
 var blankItem = new Item();
-
-blankItem.log();
-
-var it = new Item({
-        title : 'foo'
-    });
-
-it.log();
+ 
+blankItem.log(); // 'title: unknown; done: false
+ 
+var it = new Item({title : 'foo'});
+ 
+it.log(); // 'title: foo; done: false
