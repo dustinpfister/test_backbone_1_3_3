34898faebf135a40d7cec57fed442692a936f513
dustinpfister <dustin.pfister@gmail.com>
2017-11-03 12:14:18 -0400

    much work on upgrades

--- apps/idle_been/idle_been.js
+++ apps/idle_been/idle_been.js
@@ -15,6 +15,14 @@ var Idle = Backbone.Model.extend(
             upgrades_data : []// an array of data objects for each item
         },
 
+        initialize : function () {
+
+            this.upgradeReset();
+
+            console.log(this.get('upgrades_data'));
+
+        },
+
         // items that help get some more beens
         upgrades : [
 
@@ -24,13 +32,20 @@ var Idle = Backbone.Model.extend(
                 id : 'clone_self',
                 desc : 'clone yourself',
 
+                // defaults for the upgrade state
+                defaults : {
+
+                    cost : 10
+
+                },
+
                 // if the function returns true
                 // the item is unlocked
-                unlockIf : function () {
+                unlockIf : function (model) {
 
                     // this allows for be to define a condition
                     // that will allow for the item to be unlokced
-                    if (this.get('beens') >= 10) {
+                    if (model.get('beens') >= 10) {
 
                         return true;
 
@@ -41,22 +56,112 @@ var Idle = Backbone.Model.extend(
                 },
 
                 // an initialize method for the item
-                init : function () {}
+                init : function (model) {
+
+                    var data = model.getUpgradeData(this.id),
+
+                    upData,
+
+                    // make a clone of the defaults for this upgrade
+                    state = _.clone(this.defaults);
+
+                    // the number of times upgraded should always be 0
+                    state.count = 0;
+
+                    // set the id, and desc to the values of this upgrade
+                    state.id = this.id;
+                    state.desc = this.desc;
+
+                    // if we have a data object all ready
+                    if (data) {
+
+                        // just set to the new state
+                        data = state;
+
+                    } else {
+
+                        // else push in a new one
+                        upData = model.get('upgrades_data');
+
+                        upData.push(state);
+
+                        model.set('upgrades_data', upData);
+
+                    }
+
+                }
 
             }
 
         ],
 
+        // get the upgrade data object for the given id
+        getUpgradeData : function (id) {
+
+            var data = this.get('upgrades_data'),
+            i = 0,
+            len = data.length;
+            while (i < len) {
+
+                if (data[i].id === id) {
+
+                    return data[i];
+
+                }
+
+                i += 1;
+            }
+
+            return false;
+
+        },
+
+        // reset the data objects for a given upgrade id or all
+        upgradeReset : function (id) {
+
+            var model = this;
+
+            // reset all
+            this.upgrades.forEach(function (upgrade) {
+
+                upgrade.init(model);
+
+            });
+
+        },
+
+        // user upgrade for given id
+        user_upgrade : function (id) {
+
+            console.log('yes this is model');
+            console.log(id);
+
+            var data = this.getUpgradeData(id);
+
+            if (data) {
+
+                data.count += 1;
+				
+				console.log(data);
+
+            } else {
+
+                console.log('data for upgrade not found!');
+
+            }
+
+        },
+
         // update items_unlocked array
         unlockedCheck : function () {
 
-            var self = this,
+            var model = this,
 
             unlocked = [];
 
             this.upgrades.forEach(function (upgrade) {
 
-                if (upgrade.unlockIf.call(self)) {
+                if (upgrade.unlockIf(model)) {
 
                     unlocked.push(upgrade.id);
 
--- apps/idle_been/idle_been_view.js
+++ apps/idle_been/idle_been_view.js
@@ -10,6 +10,8 @@ var View = Backbone.View.extend({
         // use a template
         dispTemplate : _.template('<p>Beens: <%- beens %><\/p><p>Unlocked: <%- upgrades_unlocked %><\/p>'),
 
+        upgradeTemplate : _.template('<li><%- desc %>; cost : <%- cost %> <input id=\"upgrade_<%- id %>\" type=\"button\" value=\"upgrade\"><\/li>'),
+
         // setup the event attachment
         events : {
 
@@ -28,8 +30,37 @@ var View = Backbone.View.extend({
         // this is called whenever the display needs to update
         render : function () {
 
+            var view = this;
+
             // update the display
             this.$el.find('#disp').html(this.dispTemplate(this.model.attributes));
+
+            var html = '';
+
+            var i = 0,
+            upData;
+
+            upData = this.model.get('upgrades_data')[i];
+            html += this.upgradeTemplate(upData);
+
+            console.log(html);
+
+            this.$el.find('#upgrades_list').html(html);
+
+            (function () {
+
+                $('#upgrade_' + upData.id).get(0).addEventListener('click', function () {
+
+                    console.log('user upgrade for: ' + upData.id);
+
+                    view.model.user_upgrade(upData.id);
+
+                });
+
+            }
+                (upData))
+
+            console.log($('#upgrade_' + upData.id).get(0))
 
         },
 
