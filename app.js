7fdfcc1be767dfa09e06c06f6956afad8367a9cb
dustinpfister <dustin.pfister@gmail.com>
2018-10-16 09:58:20 -0400

    using klaw to run over demo folders

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let express = require('express'),
 path = require('path'),
+klaw = require('klaw'),
+through2 = require('through2'),
 app = express(),
 
 port = process.env.PORT || process.argv[2] || 8080;
@@ -9,13 +11,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
- 
-    res.render('index',{});
- 
+
+    klaw(path.join(__dirname, 'views/demos'))
+
+    // only directories
+    .pipe(through2.obj(function (item, enc, next) {
+
+            if (item.stats.isDirectory()) {
+                this.push(item);
+            }
+            next();
+
+        }))
+
+    .on('data', function (item) {
+
+        console.log(item.path);
+
+    })
+
+    .on('end', function () {
+
+        res.render('index', {});
+
+    });
+
 });
- 
+
 app.listen(port, function () {
- 
+
     console.log('app is up on port: ' + port);
- 
+
 });
